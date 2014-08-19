c2e9a4289456edcc70f9ecce3b4cc53ace67ea33
Chao Zhang <chao.a.zhang@intel.com>
2014-08-19 15:00:21 +0800

    Rewrite weather app, in order to support: animation can be disturbed and response touch event as soon as possible Perfect implementation. Other apps can reference this implementation

--- app/weather/app.js
+++ app/weather/app.js
@@ -7,31 +7,72 @@ var sys = require('../../main/highLevelAPI/sys.js');
 
 var logPrefix = '[user weather] ';
 
+var isAnimationDispComplete = true;
+var isPreviousImageDisComplete = true;
+var imageIter = -1;
+
+var imgs = null;
 function displayWeather() {
+  if (!isAnimationDispComplete) {return;}
+  isAnimationDispComplete = false;
+  isPreviousImageDisComplete = true;
+  imageIter = -1;
   var w = fs.readFileSync(path.join(__dirname, '.\/weather_from_baidu.json'), 'utf8');
   if (w == '') {
     // Display an img, no weather information
     var weather = fs.readFileSync(path.join(__dirname, '.\/no_weather_info_media.json'), 'utf8');
-    disp(JSON.parse(weather));
-    setTimeout(displayWeather, 100);
+    imgs = JSON.parse(weather);
   } else {
     if (fs.existsSync(path.join(__dirname, JSON.parse(w).weather, 'media.json'))) {
       console.log(logPrefix+'weather file exist');
       var weather = fs.readFileSync(path.join(__dirname, JSON.parse(w).weather, 'media.json'), 'utf8');
-      disp(JSON.parse(weather));
-      setTimeout(displayWeather, 100);
+      imgs = JSON.parse(weather);
     }
   }
 }
 
-var singleImageSize = 16*12/2;
-function disp(imgs) {
-  var data = [];
-  for (var imageIter=0; imageIter<imgs.numberOfImg; imageIter++) {
-    //io.disp_raw_N(imgs['img'+imageIter], 1, 100);
-    dispSingle(imgs['img'+imageIter], 1, 100);
-  }
+function dispAnimation() {
+  if (!isPreviousImageDisComplete) {return;}
+  isPreviousImageDisComplete = false;
+  imageIter++;
+  if (imageIter==imgs.numberOfImg) {isAnimationDispComplete = true; return;}
+  dispSingle(imgs['img'+imageIter], 1, 0);
+  isPreviousImageDisComplete = true;
 }
+
+setInterval(displayWeather, 100);
+setInterval(function(){dispAnimation();}, 100);
+
+/*function displayWeather() {
+  if (!isAnimationDispComplete) {setTimeout(displayWeather, 100); return;}
+  isAnimationDispComplete = false;
+  isPreviousImageDisComplete = true;
+  imageIter = -1;
+  var w = fs.readFileSync(path.join(__dirname, '.\/weather_from_baidu.json'), 'utf8');
+  if (w == '') {
+    // Display an img, no weather information
+    var weather = fs.readFileSync(path.join(__dirname, '.\/no_weather_info_media.json'), 'utf8');
+    dispAnimation(JSON.parse(weather));
+  } else {
+    if (fs.existsSync(path.join(__dirname, JSON.parse(w).weather, 'media.json'))) {
+      console.log(logPrefix+'weather file exist');
+      var weather = fs.readFileSync(path.join(__dirname, JSON.parse(w).weather, 'media.json'), 'utf8');
+      dispAnimation(JSON.parse(weather));
+    }
+  }
+  setTimeout(displayWeather, 100);
+}
+
+function dispAnimation(imgs) {
+  if (!isPreviousImageDisComplete) {setTimeout(function(){dispAnimation(imgs);}, 100); return;}
+  isPreviousImageDisComplete = false;
+  imageIter++;
+  if (imageIter==imgs.numberOfImg) {isAnimationDispComplete = true; return;}
+  dispSingle(imgs['img'+imageIter], 1, 0);
+  isPreviousImageDisComplete = true;
+  setTimeout(function(){dispAnimation(imgs);}, 100);
+}
+*/
 
 function dispSingle(data, number, interval) {
   io.disp_raw_N(data, number, interval);
