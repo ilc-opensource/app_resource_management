547ac6942a3908fbd864e60e16486f40dfcfb1da
Chao Zhang <chao.a.zhang@intel.com>
2014-08-28 20:22:11 +0800

    move fish app from setting to main app

--- main/highLevelAPI/getTouch.js
+++ main/highLevelAPI/getTouch.js
@@ -1,22 +1,23 @@
 var fs = require('fs');
 var path = require('path');
 var io = require('./io.js');
+var touchHandle = io.mug_touch_init();
 
 /*io.mug_touch_on(function(x, y, id) {
   fs.appendFileSync(path.join(__dirname, '../touchEvent.json'), JSON.stringify({'touch':[x, y, id]})+'\n');
 });
 */
 
-io.mug_touch_event_on(io.TOUCH_HOLD, function(e, x, y, id) {
+io.mug_touch_event_on(touchHandle, io.TOUCH_HOLD, function(e, x, y, id) {
   fs.appendFileSync(path.join(__dirname, '../touchEvent.json'), JSON.stringify({'touchEvent':[e, x, y, id]})+'\n');
 });
 
-io.mug_touch_event_on(io.TOUCH_CLICK, function(e, x, y, id) {
+io.mug_touch_event_on(touchHandle, io.TOUCH_CLICK, function(e, x, y, id) {
   fs.appendFileSync(path.join(__dirname, '../touchEvent.json'), JSON.stringify({'touchEvent':[e, x, y, id]})+'\n');
 });
 
-io.mug_gesture_on(io.MUG_GESTURE, function(g, info) {
+io.mug_gesture_on(touchHandle, io.MUG_GESTURE, function(g, info) {
   fs.appendFileSync(path.join(__dirname, '../touchEvent.json'), JSON.stringify({'gesture':g})+'\n');
 });
 
-io.mug_run_touch_thread();
+io.mug_run_touch_thread(touchHandle);
