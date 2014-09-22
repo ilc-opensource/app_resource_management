55fbf7510e6ee3d63c1f98a433aa86c09ed5542b
czhan25 <chao.a.zhang@intel.com>
2014-09-22 13:39:01 +0800

    Add twitter

---
+++ app/twitter/app.js
@@ -0,0 +1,119 @@
+var fs = require('fs');
+var child_process = require('child_process');
+var path = require('path');
+var http = require('http');
+
+var io = require('../../main/highLevelAPI/io.js');
+var sys = require('../../main/highLevelAPI/sys.js');
+
+var logPrefix = '[userApp twitter] ';
+
+var getTwitterProcess = null;
+var content = '';
+var handler = function(o) {
+  if (o['twitter']) {
+    twitterContent = o['twitter'];
+  }
+};
+var hasContent = false;
+var currentDispContent = null;
+
+// Animation display begin
+var isAnimationDispComplete = true;
+var isPreviousImageDisComplete = true;
+var imageIter = -1;
+var imgs = null;
+function display() {
+  if (!isAnimationDispComplete) {
+    setTimeout(display, 500);
+    return;
+  }
+  isAnimationDispComplete = false;
+  // The animation is only one image, don't need to refresh it
+  if (currentDispContent == content &&
+    JSON.parse(currentDispContent).numberOfImg == 1) {
+    isAnimationDispComplete = true;
+    setTimeout(display, 500);
+    return;
+  }
+  if (content == '' || content == '\n') {
+    var w = fs.readFileSync(path.join(__dirname, './loading.json'), 'utf8');
+    imgs = JSON.parse(w);
+    hasContent = false;
+  } else {
+    try {
+      imgs = JSON.parse(content);
+      hasContent = true;
+      currentDispContent = content
+    } catch(ex) {
+      imgs = null;
+      isAnimationDispComplete = true;
+      isPreviousImageDisComplete = false;
+      hasContent = false;
+      setTimeout(display, 500);
+      return;
+    }
+  }
+  isPreviousImageDisComplete = true;
+  imageIter = -1;
+  setTimeout(display, 500);
+}
+function dispAnimation() {
+  if (!isPreviousImageDisComplete) {
+    setTimeout(dispAnimation, 50);
+    return;
+  }
+  isPreviousImageDisComplete = false;
+  imageIter++;
+  if (content != '' && !hasContent) { // Terminate loading animation immediately
+    isAnimationDispComplete = true;
+    setTimeout(dispAnimation, 50);
+    return;
+  }
+  if (currentDispContent != null &&
+    currentDispContent != content) {
+    for (var i=0; i<imgs.textEnd.length; i++) {
+      if ((imageIter-1) == imgs.textEnd[i]) {
+        isAnimationDispComplete = true;
+        setTimeout(dispAnimation, 50);
+        return;
+      }
+    }
+  }
+  if (imageIter>=imgs.numberOfImg) {
+    isAnimationDispComplete = true;
+    setTimeout(dispAnimation, 50);
+    return;
+  }
+  dispSingle(imgs['img'+imageIter], 1, 50);
+  isPreviousImageDisComplete = true;
+  setTimeout(dispAnimation, 50);
+}
+function dispSingle(data, number, interval) {
+  io.disp_raw_N(data, number, interval);
+}
+// Animation display End
+
+var twitter = function() {
+  getTwitterProcess = child_process.fork(path.join(__dirname, 'getTwitter.js'));
+  getTwitterProcess.on('message', handler);
+  display();
+  dispAnimation();
+};
+
+twitter();
+
+//setInterval(display, 100);
+//setInterval(function(){dispAnimation();}, 100);
+
+// Touch event handler begin
+io.touchPanel.on('touchEvent', function(e, x, y, id) {
+  //if (e == 'TOUCH_HOLD') {
+  //  sys.escape();
+  //}
+});
+
+io.touchPanel.on('gesture', function(gesture) {
+  //console.log(logPrefix+'getsture='+gesture);
+});
+// Touch event handler end
---
+++ app/twitter/getWeChat.js
@@ -0,0 +1,81 @@
+var fs = require('fs');
+var child_process = require('child_process');
+var path = require('path');
+var http = require('http');
+
+var io = require('../../main/highLevelAPI/io.js');
+var sys = require('../../main/highLevelAPI/sys.js');
+
+var logPrefix = '[user weChat getWeChat] ';
+
+var lastMsg = null;
+function action(msg) {
+  if (msg=='') return;
+  if (lastMsg != msg) {
+    lastMsg = msg;
+    //console.log(msg);
+
+    process.send({'weChat':msg});
+
+    sys.registerNotification(path.join(__dirname, 'media.json'), path.join(__dirname, 'app.js'));
+
+/*    fs.writeFile(path.join(__dirname, 'weChat.json'),
+      msg,
+      function(err) {
+        if(err)
+          throw err;
+        //console.log('It\'s saved!');
+      }
+    );
+*/
+  }
+}
+
+function queryweChat(cb) {
+  var mugID = 'MUG123456ILC';
+  var app = 'weChat';
+
+  var optionsProxy = {
+    hostname: 'proxy-prc.intel.com',
+    port: 911,
+    path: 'www.pia-edison.com/mug?mugID='+mugID+'&app='+app,
+    method: 'GET'
+  };
+
+  var options = {
+    hostname: 'www.pia-edison.com',
+    port: 80,
+    path: '/mug?mugID='+mugID+'&app='+app,
+    method: 'GET'
+  };
+
+  //var req = http.request(optionsProxy, function(res) {
+  var req = http.request(options, function(res) {
+    res.setEncoding('utf8');
+    var body = '';
+    res.on('data', function (chunk) {
+      body += chunk;
+    });
+    res.on('end', function () {
+      if (!body.match(/^</)) {
+        cb(body);
+      }
+    });
+  });
+  req.on('socket', function (socket) {
+    socket.setTimeout(2000);
+    socket.on('timeout', function() {
+        req.abort();
+    });
+  });
+  req.on('error', function(e) {
+    //console.log('problem with request: ' + e.message);
+  });
+  req.end();
+}
+// Query info from web end
+
+//var weChatContent = fs.readFileSync(path.join(__dirname, './weChat.json'), 'utf8');
+//process.send({'weChat':weChatContent});
+
+setInterval(function(){queryweChat(action)}, 1000);
