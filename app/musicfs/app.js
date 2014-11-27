43d2d1e08c78694f7b3c582015989bf250014324
czhan25 <chao.a.zhang@intel.com>
2014-11-26 22:19:02 -0500

    Fix Chinese display in JS

--- app/musicfs/app.js
+++ app/musicfs/app.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var child_process = require('child_process');
+var utf8 = require('utf8');
 
 var io = require('../../main/highLevelAPI/io.js');
 var sys = require('../../main/highLevelAPI/sys.js');
@@ -9,10 +10,13 @@ var logPrefix = '[sys show app] ';
 var appReady = false;
 var index = 0;
 var musicFile = [];
+var audioDir = '/home/root/audio/'
 
-function delFile () {
-  //musicFile = fs.readdirSync(path.join(__dirname, '../player/music/'));
-  musicFile = fs.readdirSync('/home/root/audio/');
+function audioFS () {
+  var rawMusicFile = fs.readdirSync(audioDir);
+  for (var i=0; i<rawMusicFile.length; i++) {
+    musicFile.push(utf8.encode(rawMusicFile[i]));
+  }
   for (var i=0; i<musicFile.length; i++) {
     if (!musicFile[i].match(/.mp3$/)) {
       musicFile.splice(i, 1);
@@ -25,63 +29,7 @@ function delFile () {
   }
 }
 
-delFile();
-
-/*function disp_app(force) {
-  var data = fs.readFileSync(path.join(__dirname, 'app.json'), 'utf8');
-  if (data == '') {
-    if (!force) {
-      setTimeout(disp_app, 300);
-    }
-    return;
-  }
-  appJSON = JSON.parse(data);
-
-  var currentApp = index==-1?null:appKey[index];
-  //console.log(logPrefix+'currentApp='+currentApp);
-  index = -1;
-  appKey = [];
-  for (var key in appJSON) {
-    if (appJSON[key].name && appJSON[key].icon) {
-      appKey.push(key);
-      if (key == currentApp) {
-        index = appKey.length-1;
-      }
-    }
-  }
-  // No app installed
-  if (appKey.length == 0) {
-    index = -1;
-  } else {
-    // The previous shown app is uninstalled, chose the first app as the new shown app
-    if (index == -1) {
-      index = 0;
-    }
-    //console.log(logPrefix+'new current app='+appKey[index]);
-  }
-  //console.log(logPrefix+'app length='+appKey.length);
-  disp();
-  if (!force) {
-    setTimeout(disp_app, 300);
-  }
-}
-
-function disp() {
-  if (index == -1) {
-    var img = path.join(__dirname, './image/none_app.jpg');
-  } else {
-    var img = path.join(__dirname,
-      '../app/',
-      appJSON[appKey[index]].name,
-      appJSON[appKey[index]].icon);
-  }
-  
-  io.disp_N([img], 1, 0);
-  appReady = true;
-}
-
-disp_app();
-*/
+audioFS();
 
 io.touchPanel.on('touchEvent', function(e, x, y, id) {
   if (e == 'TOUCH_HOLD') {
@@ -91,16 +39,16 @@ io.touchPanel.on('touchEvent', function(e, x, y, id) {
 
 io.touchPanel.on('gesture', function(gesture) {
   if (gesture == 'MUG_SWIPE_UP') {
+    if(musicFile.length != 0) {
+      child_process.exec('rm '+path.join(audioDir, musicFile[index]));
+      musicFile.splice(index, 1);
 
-    if(musicFile.length != 0)
-      child_process.exec('rm '+path.join(__dirname, '../player/music/', musicFile[index]));
-
-    musicFile.splice(index, 1);
-    index = 0;
-    if (musicFile.length==0) {
-      io.disp_text_marquee_async('No music file', 100);
-    } else {
-      io.disp_text_marquee_async(musicFile[index], 100);
+      index = 0;
+      if (musicFile.length==0) {
+        io.disp_text_marquee_async('No music file', 100);
+      } else {
+        io.disp_text_marquee_async(musicFile[index], 100);
+      }
     }
   } else if (gesture == 'MUG_SWIPE_RIGHT') {
     index = (index==0)?(musicFile.length-1):(index-1);
