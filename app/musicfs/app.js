cb72e6c570c8fc3794dbba740a87f79b460310bf
czhan25 <chao.a.zhang@intel.com>
2014-12-26 15:33:01 +0800

    Fix bug in musicFS, Chinese decoder

--- app/musicfs/app.js
+++ app/musicfs/app.js
@@ -10,16 +10,17 @@ var logPrefix = '[sys show app] ';
 var appReady = false;
 var index = 0;
 var musicFile = [];
+var rawMusicFile = null;
 var audioDir = '/home/root/audio/'
 
 function audioFS () {
-  var rawMusicFile = fs.readdirSync(audioDir);
+  rawMusicFile = fs.readdirSync(audioDir);
   for (var i=0; i<rawMusicFile.length; i++) {
     if (rawMusicFile[i].match(/.mp3$/)) {
       // Chmod
       fs.chmodSync(path.join(audioDir, rawMusicFile[i]), 0600);
       // Put into array
-      musicFile.push(utf8.encode(rawMusicFile[i]));
+      musicFile.push({"file":utf8.encode(rawMusicFile[i]), '_index':i});
     }
   }
   /*for (var i=0; i<musicFile.length; i++) {
@@ -35,7 +36,7 @@ function audioFS () {
   if (musicFile.length==0) {
     io.disp_text_marquee_async('No music file', 'red', 100, -1);
   } else {
-    io.disp_text_marquee_async(musicFile[index], 'blue', 100, -1);
+    io.disp_text_marquee_async(musicFile[index].file, 'blue', 100, -1);
   }
 }
 
@@ -51,21 +52,22 @@ io.touchPanel.on('gesture', function(gesture) {
   if (gesture == 'MUG_SWIPE_UP') {
     if(musicFile.length != 0) {
       //child_process.exec('rm '+path.join(audioDir, musicFile[index]));
-      child_process.exec('rm '+audioDir+'"'+musicFile[index]+'"', function(e, o, s){console.log(e+o+s);});
+      child_process.exec('rm '+audioDir+'"'+rawMusicFile[musicFile[index]._index]+'"', function(e, o, s){console.log(e+o+s);});
       musicFile.splice(index, 1);
 
       index = 0;
       if (musicFile.length==0) {
         io.disp_text_marquee_async('No music file', 'red', 100, -1);
       } else {
-        io.disp_text_marquee_async(musicFile[index], 'blue', 100, -1);
+        io.disp_text_marquee_async(musicFile[index].file, 'blue', 100, -1);
       }
     }
   } else if (gesture == 'MUG_SWIPE_RIGHT') {
     index = (index==0)?(musicFile.length-1):(index-1);
-    io.disp_text_marquee_async(musicFile[index], 'blue', 100, -1);
+    io.disp_text_marquee_async(musicFile[index].file, 'blue', 100, -1);
   } else if (gesture == 'MUG_SWIPE_LEFT') {
     index = (index+1)==musicFile.length?0:(index+1);
-    io.disp_text_marquee_async(musicFile[index], 'blue', 100, -1);
+    io.disp_text_marquee_async(musicFile[index].file, 'blue', 100, -1);
   }
 });
+
